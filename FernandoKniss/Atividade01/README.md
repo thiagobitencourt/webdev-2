86879aa518537da18e51e9d7003d9765a182b197
FernandoKniss <fernandofkniss@gmail.com>
2017-04-20 19:30:24 -0300

    Questão 1 respondida
    Respondido com sucesso por Fernando Furtado Kniss.
    thiagobitencourt/webdev-2#29

---
+++ FernandoKniss/Atividade01/README.md
@@ -0,0 +1,10 @@
+# Atividade 01 -  Fernando Furtado Kniss
+
+## Questões
+
+### 1. Quais são as duas formas de se obter um repositório git?
+
+-git init: coloca o diretórial sobre controle do git.
+-git clone: faz o clone de um repositório existente.
+
+ 
---
+++ FernandoKniss/nodeExemplo.js
@@ -0,0 +1,32 @@
+'use strict';
+
+//console.log("Olá mundo");
+
+var x = 10;
+var y = "qualquercoisa";
+
+//let z = 10;
+//let k = "outra string";
+
+const c = 10;
+// c = 20;
+//console.log(c);
+
+
+  //funçao
+function liberarOsAlunoDaAula(algumNome) {
+ console.log(algumNome);
+  return 20;
+}
+
+var resposta = liberarOsAlunoDaAula(50);
+// console.log(resposta);
+
+var func2 = function(a) {
+console.log(a);
+}
+
+func2("oi");
+
+var minhaFuncao = liberarOsAlunoDaAula;
+minhaFuncao("Não");
