60c6012e499dc15b3751717a35ce02bc428ce084
Eleani <eleani.kreutzfeld@gmail.com>
2017-04-20 21:21:21 -0300

    Respondido a questão 7
    thiagobitencourt/webdev-2#29

--- EleaniKreutzfeld/Atividade01/Atividade.js
+++ EleaniKreutzfeld/Atividade01/Atividade.js
@@ -18,3 +18,22 @@ const foo = {
   atributoInt: 50,
   atributoArray: []
 };
+
+/*
+###7. Usando a sintaxe da linguagem JavaScript, defina uma
+função que recebe como parâmetro dois valores e que
+retorna um objeto que armazena os valores recebidos
+nos atributos a e b.
+Execute esta função e imprima o resultado no console.
+*/
+
+function funcaoTeste(val1, val2) {
+  var objetoRetorno = {
+    a:val1,
+    b:val2
+  };
+  return objetoRetorno;
+}
+
+var valorRetorno = funcaoTeste(5, 'teste');
+console.log(valorRetorno);
