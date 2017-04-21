f9e9c7b07645287511d28e94acec62b0d74be925
FernandoKniss <fernandofkniss@gmail.com>
2017-04-20 21:22:17 -0300

    Questão 7 respondido

--- FernandoKniss/Atividade01/Atividade.js
+++ FernandoKniss/Atividade01/Atividade.js
@@ -12,3 +12,20 @@ const foo = {
   atributoArray: [ ],
   atributoFunc: function() {}
 };
+
+/*
+7. Usando a sintaxe da linguagem JavaScript, defina uma função que recebe como
+parâmetro dois valores e que retorna um objeto que armazena os valores recebidos
+nos atributos a e b. Execute esta função e imprima o resultado no console.
+*/
+
+function questaoSete(valor1, valor2) {
+  var objetoDeRetorno = {
+    a: valor1,
+    b: valor2
+  };
+  return objetoDeRetorno;
+}
+
+var retornoObjeto = questaoSete(5, "seis");
+console.log(retornoObjeto);
