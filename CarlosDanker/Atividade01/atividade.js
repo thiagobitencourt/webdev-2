eff768e8cf32804b68022b45e54af8b630c9db46
carlosdanker <carlos.danker@gmail.com>
2017-04-20 21:20:50 -0300

    Resposta para questao 7

--- CarlosDanker/Atividade01/atividade.js
+++ CarlosDanker/Atividade01/atividade.js
@@ -12,3 +12,22 @@ const foo = {
   number: 41,
   array: []
 };
+
+/*
+### 7. Usando a sintaxe da linguagem JavaScript,
+defina uma função que recebe como parâmetro dois
+valores e que retorna um objeto que armazena os
+valores recebidos nos atributos a e b.
+Execute esta função e imprima o resultado no console.
+*/
+
+function questaoSete(valor1, valor2){
+  var objetoDeRetorno = {
+    a: valor1,
+    b: valor2
+  };
+  return objetoDeRetorno
+}
+
+var executaQuestao = questaoSete(5, 'seis');
+console.log(executaQuestao)
