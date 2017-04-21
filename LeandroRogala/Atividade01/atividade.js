e69b40a13a8e70a517954eefe9fdec39bca04643
Leandro Rogala Britez <leandrorogala17@hotmail.com>
2017-04-20 21:21:24 -0300

    Responde questão numero 7
    a resposta em codigo se encontra dentro do arquivo atividade.js
    
    thiagobitencourt/webdev-2#29

--- LeandroRogala/Atividade01/atividade.js
+++ LeandroRogala/Atividade01/atividade.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /*
-Usando a sintaxe da linguagem JavaScript, crie um objeto com um atributo que tenha um valor do tipo string,
+6) Usando a sintaxe da linguagem JavaScript, crie um objeto com um atributo que tenha um valor do tipo string,
 um atributo do tipo number e um atributo com valor do tipo array.Atribua este objeto a uma constante.
 */
 
@@ -10,3 +10,21 @@ const objeto = {
   atributoInt: 5,
   atributoArray: []
 };
+
+/*
+ 7) Usando a sintaxe da linguagem JavaScript, defina uma função que recebe como parâmetro dois valores e
+ que retorna um objeto que armazena os valores recebidos nos atributos a e b. Execute esta função e imprima o resultado no console.
+ */
+
+ function funcao(valor1, valor2){
+
+    var objeto = {
+      atributoA: valor1,
+      atributoB: valor2
+    }
+
+    return objeto;
+ }
+
+ var valorRetorno = funcao(5, "seis");
+ console.log(valorRetorno);
