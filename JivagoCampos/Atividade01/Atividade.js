d00d45cfb9a1d1e1db7b3aad4725572d67a52c89
jivagocampos <jivagocampos_@hotmail.com>
2017-04-21 17:06:01 -0300

    Resposta da questão 7
     thiagobitencourt/webdev-2#29

--- JivagoCampos/Atividade01/Atividade.js
+++ JivagoCampos/Atividade01/Atividade.js
@@ -12,3 +12,20 @@ const foo = {
   atributoArray:[],
   atributoFunc: function () { }
 };
+
+/*
+Questão 7:
+Usando a sintaxe da linguagem JavaScript, defina uma função que recebe como
+parâmetro dois valores e que retorna um objeto que armazena os valores recebidos
+nos atributos a e b. Execute esta função e imprima o resultado no console.
+*/
+
+function questaoSete(valor1, valor2) {
+  var objetoDeRetorno = {
+    a: valor1,
+    b: valor2
+  };
+  return objetoDeRetorno;
+  }
+  var valorRetorno  questaoSete(5, "seis");
+  console.log(valorRetorno);
