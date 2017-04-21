73998a918c900a7c27c11ee1d85b3009b4e7021a
willian mendes <willink01@hotmail.com>
2017-04-20 21:12:18 -0300

    questao 6s

---
+++ WillianMendes/Atividade01/atividade.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/* questao 6:
+ Usando a sintaxe da linguagem JavaScript, 
+crie um objeto com um atributo que tenha um valor do tipo string,
+um atributo do tipo number e um atributo com valor do tipo array.
+Atribua este objeto a uma constante.
+*/
+
+const objeto = {
+	valor:10,
+	nome:"texto",
+	matriz:[2];
+};
