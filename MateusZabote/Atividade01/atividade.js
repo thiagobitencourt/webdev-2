9d505ab236c6908ff0c7f5d95a7430fdcf3483ad
MateusZabote <mateus.zabotedasilva3@gmail.com>
2017-04-20 21:21:41 -0300

    Resposta referente a questão 07

--- MateusZabote/Atividade01/atividade.js
+++ MateusZabote/Atividade01/atividade.js
@@ -9,5 +9,22 @@ const foo = {
   chave: 'valor string',
   atributoInt: 33,
   atributoArray: [],
-  atributoFunc: function(){}  
+  atributoFunc: function(){}
 };
+
+/*
+Questão 7:
+Usando a sintaxe da linguagem JavaScript, defina uma função que recebe como parâmetro dois valores e que retorna
+um objeto que armazena os valores recebidos nos atributos a e b. Execute esta função e imprima o resultado no console.
+*/
+
+function questaoSete(valor1, valor2){
+  var objetoDeRetorno = {
+    a:valor1,
+    b:valor2
+  };
+  return objetoDeRetorno;
+}
+
+var valorRetorno = questaoSete(5, "seis");
+console.log(valorRetorno);
