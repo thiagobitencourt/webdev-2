f89338ccf5bcfa026eacf1fd5da1e98d44657779
Leandro Rogala Britez <leandrorogala17@hotmail.com>
2017-04-20 21:11:42 -0300

    Responde questão numero 6
    A resposta da questão esta dentro do arquivo atividade.js
    
    thiagobitencourt/webdev-2#29

---
+++ LeandroRogala/Atividade01/atividade.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/*
+Usando a sintaxe da linguagem JavaScript, crie um objeto com um atributo que tenha um valor do tipo string,
+um atributo do tipo number e um atributo com valor do tipo array.Atribua este objeto a uma constante.
+*/
+
+const objeto = {
+  atributoString: 'String',
+  atributoInt: 5,
+  atributoArray: []
+};
