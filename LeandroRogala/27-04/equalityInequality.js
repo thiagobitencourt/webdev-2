d9e04097deb3d9adcb75dbf3ddce937329eee551
Leandro Rogala Britez <leandrorogala17@hotmail.com>
2017-04-27 21:28:19 -0300

    Adiciona arquivo equalityInequality, hoisting e closures

---
+++ LeandroRogala/27-04/equalityInequality.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var a = 42;
+var b = "72";
+
+//Equality
+a == b; //Coercion
+a === b;
+// Not Equality
+a != b:
+a !==b;
+
+// Inequality
+var ehMaior = a > b; //Boolean
+var ehMenor = a < b; //Boolean
+
+// Usado para números
+// Também usado para String;
+
+var str1 = "casa";
+var str2 = "abacaxi";
+
+str1 > str2; // true ou false
