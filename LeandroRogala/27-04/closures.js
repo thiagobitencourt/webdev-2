22aaab94d29c98a034385e190a6f156b39d1209a
Leandro Rogala Britez <leandrorogala17@hotmail.com>
2017-04-27 21:51:48 -0300

    Modifica closures.js

--- LeandroRogala/27-04/closures.js
+++ LeandroRogala/27-04/closures.js
@@ -1,13 +1,34 @@
 'use strict'
 
-function funcA(){
-  var x = 10;
-  return function(xpto){
-    console.log(xpto + x);
+function criaSomador(somar){
+  var x = somar;
+
+  var somador = function(valor){
+    return valor + x;
+  }
+
+  return somador;
+  }
+
+var soma10 = criaSomador(10);
+var resultado = soma10(16);
+
+var soma500 = criaSomador(500);
+
+console.log(soma500(10));
+console.log(soma10(20));
+
+function fabrica(){
+
+  var obj = {
+    criaUmacoisa: funcaoCriaUmaCoisa,
+    criaOutraCoisa: funcaoCriaOutraCoisa
+  };
+
+  return obj;
+
+  function funcaoCriaUmaCoisa(a){
+  }
+  function funcaoCriaOutraCoisa(b){
   }
 }
-
-var soma10 = funcA();
-soma10(5); // ->15
-
-// (function () {}) ();
