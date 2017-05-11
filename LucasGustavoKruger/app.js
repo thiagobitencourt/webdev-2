01407b29e8d96b2594880acb275cac4229272371
Lucas <lucas.kr1996@gmail.com>
2017-05-11 19:41:20 -0300

    Adiciona body-parser ao projeto

--- LucasGustavoKruger/app.js
+++ LucasGustavoKruger/app.js
@@ -3,7 +3,10 @@
 const port = 3000;
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.json())
 
 app.get('/', function(req, res) {
   console.log('Alguém chamou /');
@@ -17,6 +20,11 @@ app.get('/usuario', function(req, res) {
     password: 'senha'
   }
   res.send(usuario);
+});
+
+app.post('/usuario', (req, res) => {
+  console.log(req.body);
+  res.send('Fine!');
 });
 
 app.listen(port, function() {
