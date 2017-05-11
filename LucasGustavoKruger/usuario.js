1c51684d859b7eb8f08948790450f5a5819bdcd5
Lucas <lucas.kr1996@gmail.com>
2017-05-11 19:25:22 -0300

    Exporta modulo de usuario

--- LucasGustavoKruger/usuario.js
+++ LucasGustavoKruger/usuario.js
@@ -1,19 +1,35 @@
 'use strict';
-
+// objeto usuario
 const usuario = {
-  username: 'Lucas Kruger',
+  username: 'LucasKr',
   password: 'lucas123',
   email: 'lucas.kr1996@gmail.com',
   age: 20
 };
 
-function Usuario() {
-  
-  this.autenticar = function(user) {
-    console.log(user);
+function Usuario() { 
+
+  var funcaoPrivada = function() {
+    console.log("aqui é privado");
+  } 
+
+  this.id = 1;
+
+  this.autenticarUsuario = function(user) { 
+    console.log("autenticar usuario"); 
+    return (
+      user.username === usuario.username &&
+      user.password === usuario.password
+    )
   }
 
-}
+  this.criarUsuario = function(user) {
+    console.log("criar usuario");
+  }
 
-let usu = new Usuario();
-usu.autenticar(usuario);
+  this.obterUsuario = function() {
+    console.log("obter usuario");
+  }
+} 
+
+module.exports = Usuario;
