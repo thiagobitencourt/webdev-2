2515a036bf15ebedf082067ea0ea74bc087e343d
eduardosmatheus <matheus-edu@outlook.com>
2017-05-14 16:37:12 -0300

    Configura endpoints para atuar em uma base de dados MongoDB.
    Contribui para a issue thiagobitencourt/webdev-2#47

--- MatheusEduardo/app.js
+++ MatheusEduardo/app.js
@@ -4,18 +4,68 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const productRepo = require('./product');
 const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
 
 const app = express();
-const database = MongoClient.connect("mongodb://localhost:27017/product_api");
 
-app.use(bodyParser.json());
+const dbUser = 'matheus';
+const dbPassword = 'baguvix';
+const dbUrl = `mongodb://${dbUser}:${dbPassword}@ds159880.mlab.com:59880/product-api`;
 
-app.get('/products', productRepo.findAll);
+MongoClient.connect(dbUrl, (err, db) => {
+  if(err) {
+    throw err;
+  }
+  const products = db.collection('products');
+  app.use(bodyParser.json());
 
-app.get('/products/:id', productRepo.findOne);
+  app.get('/', (req, res) => {
+    res.send('This is the Product API.');
+  });
 
-app.post('/products', productRepo.insert);
+  app.get('/products', (req, res) => {
+    products.find().toArray((err, results) => {
+      res.send(results);
+    });
+  });
 
-app.put('/products/:id', productRepo.update);
+  app.post('/products', (req, res) => {
+    
+    const newProduct = Object.assign({}, req.body, {
+      _id: new ObjectID()
+    });
 
-app.delete('/products/:id', productRepo.delete);
+    products.save(newProduct, (err, result) => {
+      if(err) {
+        throw err;
+      }
+      console.log(`${req.body.nome} saved successfully!`);
+      res.redirect('/');
+    });
+  });
+
+  app.get('/products/:id', (req, res) => {
+    products.findOne({ _id: req.params.id }, (err, item) => {
+      res.send(item);
+    });
+  });
+
+  app.put('/products/:id', (req, res) => {
+    
+    products.updateOne({ _id: req.body._id }, req.body, (err, result) => {
+      if (err) {
+        throw err;
+      }
+      console.log(`Product ${updatedProduct._id} updated successfully!`)
+      res.send(result);
+    });
+  });
+
+  app.delete('/products/:id', (req, res) => {
+    products.deleteOne({ _id: req.params.id }).then((result) => {
+      console.log(result);
+    });
+  });
+
+  app.listen(8080, () => console.log('Listening on port 8080'));
+});
