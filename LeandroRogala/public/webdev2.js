685b6bc249f886e70c3a2010ef9c4f777fbbc886
Leandro Rogala Britez <leandrorogala17@hotmail.com>
2017-05-18 20:56:41 -0300

    adiciona botoes com bootstrap

--- LeandroRogala/public/webdev2.js
+++ LeandroRogala/public/webdev2.js
@@ -2,6 +2,13 @@
 
 var app = angular.module('webdev-2', []);
 app.controller('mainController', function($scope){
+  $scope.usuario = {
+    id: 1,
+    username: "ximbinha",
+    password: "s2joelma",
+    age: 18,
+    email: "ximbinha@joelma"
+  }
   $scope.titulo = "Usuários";
   $scope.adicionarUsuario = function(){
     console.log("adicionarUsuario");
