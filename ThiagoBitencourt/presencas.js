9923d52cb6d7d4c4ae278936ef336c61ac093382
Thiago R. M. Bitencourt <thiago.mbitencourt@gmail.com>
2017-05-18 21:44:39 -0300

    Adiciona lista de presença

--- ThiagoBitencourt/presencas.js
+++ ThiagoBitencourt/presencas.js
@@ -31,8 +31,26 @@ const presenca = [
       { aluno: "Thales", aulas: 4 },
       { aluno: "Willyan", aulas: 4 },
       { aluno: "Mateus", aulas: 4 },
-      { aluno: "Djovani", aulas: 2 },
+      { aluno: "Djovani", aulas: 3 },
       { aluno: "Alexandro", aulas: 1 }
+    ]
+  },
+  {
+    aula: "18/05/2017",
+    alunosPresentes: [
+      { aluno: "Alexandro", aulas: 4 },
+      { aluno: "Jivago", aulas: 4 },
+      { aluno: "Fernando", aulas: 4 },
+      { aluno: "Christopher", aulas: 0 },
+      { aluno: "Matheus", aulas: 4 },
+      { aluno: "Mateus", aulas: 4 },
+      { aluno: "Leandro", aulas: 4 },
+      { aluno: "Thales", aulas: 0 },
+      { aluno: "Eleani", aulas: 4 },
+      { aluno: "Willian", aulas: 4 },
+      { aluno: "Diogo", aulas: 0 },
+      { aluno: "Djovani", aulas: 4 },
+      { aluno: "Lucas", aulas: 4 }
     ]
   }
 ];
