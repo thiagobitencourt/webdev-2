874a62d08ea69ec833f41a31cc067d9d82ee99b9
Leandro Rogala Britez <leandrorogala17@hotmail.com>
2017-05-18 21:43:33 -0300

    Commit fim de aula

--- LeandroRogala/public/webdev2.js
+++ LeandroRogala/public/webdev2.js
@@ -2,21 +2,47 @@
 
 var app = angular.module('webdev-2', []);
 app.controller('mainController', function($scope){
-  $scope.usuario = {
-    id: 1,
-    username: "ximbinha",
-    password: "s2joelma",
-    age: 18,
-    email: "ximbinha@joelma"
-  }
+  var incrementaId = 1;
+  $scope.usuarios = [
+    {
+      id: incrementaId++,
+      username: "ximbinha",
+      password: "s2joelma",
+      age: 18,
+      email: "ximbinha@joelma"
+    },
+    {
+      id: incrementaId++,
+      username: "joelma",
+      password: "s2ximbinha",
+      age: 19,
+      email: "joelma@ximbinha"
+    }
+  ];
   $scope.titulo = "Usuários";
   $scope.adicionarUsuario = function(){
     console.log("adicionarUsuario");
+    $scope.adicionandoUsuario = true;
   }
   $scope.excluirUsuario = function(){
     console.log("Excluir usuario");
   }
   $scope.editarusuario = function(){
     console.log("Editar usuario");
+  }
+
+  $scope.selecionaUsuario = function(usuario){
+    $scope.usuarios.forEach(function(user){
+      user.selecionado = false;
+    });
+    usuario.selecionado = !usuario.selecionado;
+  }
+
+  $scope.salvarNovoUsuario = function(usuario){
+    console.log(usuario);
+    usuario.id = incrementaId++;
+    $scope.usuarios.push(usuario);
+    $scope.novoUsuario = null;
+    $scope.adicionandoUsuario = false;
   }
 });
