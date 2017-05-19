e016a44e437010975f992461cf3db03174450d05
Thiago R. M. Bitencourt <thiago.mbitencourt@gmail.com>
2017-05-18 21:02:11 -0300

    Adicionar ng-repeat

--- ThiagoBitencourt/public/webdev2.js
+++ ThiagoBitencourt/public/webdev2.js
@@ -4,6 +4,29 @@ var app = angular.module('webdev-2', []);
 app.controller('mainController', function($scope) {
 
   $scope.titulo = "Usuários";
+  $scope.usuarios = [
+    {
+      id: 2,
+      username: "chiquinha",
+      password: "chavesquerido",
+      age: 7,
+      email: "chiquinha@vila.com"
+    },
+    {
+      id: 1,
+      username: "jaime",
+      password: "jaime123",
+      age: 18,
+      email: "jai@me.com"
+    },
+    {
+      id: 3,
+      username: "tibursio",
+      password: "12345",
+      age: 89,
+      email: "tibursio@quase.com"
+    }
+  ];
 
   $scope.usuario = {
     id: 1,
