b0b5ac3e6343fb1835791b7805bee202322f9e7b
eduardosmatheus <matheus-edu@outlook.com>
2017-05-21 18:47:11 -0300

    Ajusta operações de CRUD para usuários.

--- MatheusEduardo/public/webdev-2.js
+++ MatheusEduardo/public/webdev-2.js
@@ -1,27 +1,46 @@
 'use strict';
 
-angular.module('webdev-2', [])
-  
-  .controller('mainController', ($scope) => {
-
-    $scope.usuarios = []
+(() => {
+  const app = angular.module('webdev-2', []);
     
-    $scope.adicionarNovoUsuario = (usuario) => {
-      usuario.id = $scope.usuarios.length + 1;
-      console.log('cheguei nessa merda')
-      $scope.usuarios.push(usuario);
+  app.controller('mainController', ($scope) => {
+
+    $scope.usuarios = [
+      {
+        username: "Matheus",
+        password: "eduardo",
+        age: 20,
+        email: "foo@example.com"
+      }
+    ];
+    
+    $scope.adicionarNovoUsuario = () => {
+      $scope.novoUsuario.id = $scope.usuarios.length + 1;
+      $scope.usuarios.push($scope.novoUsuario);
       $scope.adicionandoNovoUsuario = false;
+      $scope.novoUsuario = {};
     }
 
     $scope.adicionarUsuario = () => {
       $scope.adicionandoNovoUsuario = true;
     }
 
-    $scope.excluirUsuario = () => {
-
-    }
+    $scope.cancelarCriacao = () => {
+      $scope.adicionandoNovoUsuario = false
+    };
 
     $scope.editarUsuario = () => {
+      
+    }
 
+    //TODO: Utilizar usuarioDAO.
+    $scope.removerUsuario = (id) => {
+      const currentUser = $scope.usuarios.find(user => user.id === id);
+      if(currentUser) {
+        $scope.usuarios.splice($scope.usuarios.indexOf(currentUser), 1);
+        return true;
+      }
+      return false;
     }
   });
+})();
