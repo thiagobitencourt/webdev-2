6c59b9eced62fbf6110e81d5a8bddfe807621c1d
eduardosmatheus <matheus-edu@outlook.com>
2017-05-21 13:38:51 -0300

    Utiliza AngularJS para controlar ações do front-end.
    Contribui para a issue thiagobitencourt/webdev-2#55

--- MatheusEduardo/public/webdev-2.js
+++ MatheusEduardo/public/webdev-2.js
@@ -4,8 +4,17 @@ angular.module('webdev-2', [])
   
   .controller('mainController', ($scope) => {
 
-    $scope.adicionarUsuario = () => {
+    $scope.usuarios = []
+    
+    $scope.adicionarNovoUsuario = (usuario) => {
+      usuario.id = $scope.usuarios.length + 1;
+      console.log('cheguei nessa merda')
+      $scope.usuarios.push(usuario);
+      $scope.adicionandoNovoUsuario = false;
+    }
 
+    $scope.adicionarUsuario = () => {
+      $scope.adicionandoNovoUsuario = true;
     }
 
     $scope.excluirUsuario = () => {
