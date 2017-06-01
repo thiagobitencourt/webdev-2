e71b4d16445c4979a8008916691c1c9116be98b2
Thalles Golembiewski <gothalles@gmail.com>
2017-06-01 19:00:37 -0300

    update

---
+++ ThallesGolembiewski/Estudos/usuario.js
@@ -0,0 +1,47 @@
+'use strict';
+// Objeto usuario
+
+var usuario = {
+  username: 'gothalles',
+  password: '123456',
+  email: 'thalles@thalles.com.br',
+  age: 24
+};
+
+function Usuario(){
+  console.log("executa a função usuario");
+  var funcaoPrivada = function() {
+    console.log ("aqui e privado");
+  }
+
+  this.id = 1;
+  this.autenticarUsuario = function(user) {
+    //funcaoPrivada();
+     console.log("autenticar usuario");
+//     if(user.username === usuario.username
+//     && user.password === usuario.password) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+   return (
+     user.username === usuario.username &&
+     user.password === usuario.password
+   )
+  }
+
+  this.criarUsuario = function(user) {
+    console.log("criar usuario");
+  }
+
+  this.objetoUsuario = function() {
+    console.log("obter usuario");
+  }
+}
+
+//var usu = new Usuario ();
+//console.log(usu);
+//usu.autenticarUsuario("um nome de usuario");
+
+
+module.exports = Usuario;
