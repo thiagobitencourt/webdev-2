48999946eacb55cfcb4e179daba98cf2c3ed34fe
jivagocampos <jivagocampos_@hotmail.com>
2017-06-02 15:30:06 -0300

    Adicionando crud Produto no mongo

--- JivagoCampos/models/produtos.js
+++ JivagoCampos/models/produtos.js
@@ -1,1 +1,12 @@
-'use strict';
+'use strict'
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var produtoSchema = new Schema({
+    nome: String,
+    codigo: String,
+    quantidade: Number,
+    disponivel: Boolean
+});
+
+module.exports = mongoose.model('Produto', produtoSchema);
--- JivagoCampos/routes/produtosRoute.js
+++ JivagoCampos/routes/produtosRoute.js
@@ -1,26 +1,71 @@
 'use strict';
 
 const express = require('express');
+const mongoose = require('mongoose');
+const produto = require('../models/produtos');
 const router = express.Router();
+mongoose.connect('mongodb://localhost/CrudProduto');
 
-router.get('/produtos', function(req, res) {
-  res.send('Nenhum produto cadastrado');
+router.get('/produto', function(req, res){
+  produto.find(function(err, produtos) {
+      if(err){
+        res.send(err);
+      }else{
+        res.json(produtos);
+      }
+  });
 });
 
-router.get('/produtos/:id', function(req, res) {
-  res.send('Nenhum produto cadastrado para o ID ' + req.params.id);
+router.post('/produto', function(req, res){
+  var newProduto = new produto();
+  newProduto.nome = req.body.nome;
+  newProduto.codigo = req.body.codigo;
+  newProduto.quantidade = req.body.quantidade;
+  newProduto.disponivel = req.body.disponivel;
+
+  newProduto.save(function(error) {
+      if(error){
+        res.send(error);
+      }else{
+        res.json({ message: 'Produto salvo'});
+      }
+  });
 });
 
-router.post('/produtos', function(req, res) {
-  res.send(req.body);
+router.get('/produto/:id', function(req, res){
+  produto.findById(req.params.id, function(error, produto) {
+      if(error){
+        res.send(error);
+      }else{
+        res.json(produto);
+      }
+  });
 });
 
-router.delete('/produtos/:id', function(req, res) {
-  res.send('Ainda não é possível excluir um produto');
+router.put('/produto/:id', function(req, res){
+  produto.findById(req.params.id, function(error, produto) {
+      if(error)
+          res.send(error);
+      produto.nome = req.body.nome;
+      produto.codigo = req.body.codigo;
+      produto.quantidade = req.body.quantidade;
+      produto.disponivel = req.body.disponivel;
+      produto.save(function(error) {
+          if(error)
+              res.send(error);
+          res.json({ message: 'Produto editado'});
+      });
+  });
 });
 
-router.put('/produtos/:id', function(req, res) {
-  res.send('Ainda não é possível atualizar os dados do produto');
+router.delete('/produto/:id', function(req, res){
+  produto.remove({_id: req.params.id}, function(error){
+      if(error){
+        res.send(error);
+      }else{
+        res.json({ message: 'Produto excluido'});
+      }
+  });
 });
 
 module.exports = router;
