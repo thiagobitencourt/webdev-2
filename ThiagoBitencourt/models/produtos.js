3b41055cfc606d1187bc74581d18d0d661368aad
Thiago R. M. Bitencourt <thiago.mbitencourt@gmail.com>
2017-06-02 20:26:32 -0300

    Adiciona integração de produto com a base de dados

--- ThiagoBitencourt/models/produtos.js
+++ ThiagoBitencourt/models/produtos.js
@@ -1,1 +1,15 @@
 'use strict';
+
+// nome, codigo, quantidade, disponivel
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const produtoSchema = new Schema({
+  nome: { type: String, required: true },
+  codigo: { type: String, required: true, unique: true },
+  quantidade: Number,
+  disponivel: Boolean
+});
+
+const Produto = mongoose.model('Produto', produtoSchema);
+module.exports = Produto;
