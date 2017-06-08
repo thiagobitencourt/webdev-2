53746a73a4bb835dc391bd8ea0b6a3629f10399e
eduardosmatheus <matheus-edu@outlook.com>
2017-06-08 20:17:41 -0300

    Cria features de saque, depósito e transferência.

--- MatheusEduardo/oficial2/routes/contaBancaria.js
+++ MatheusEduardo/oficial2/routes/contaBancaria.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const ContaBancaria = require('../model/contaBancaria');
-
+const ObjectId = require('mongoose').Types.ObjectId;
 router.get('/', (req, res) => {
   ContaBancaria.find().then(results => {
     res.send(results);
@@ -10,16 +10,18 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
   const newContaBancaria = new ContaBancaria({
+    _id: new ObjectId(),
     titular: req.body.titular,
     cpf: req.body.cpf,
     email: req.body.email,
     numero: req.body.numero,
     saldoAtual: req.body.saldoAtual
   });
-
-  newContaBancaria.save().then(result => {
-    console.log(result);
-    ContaBancaria.find().then(results => {
+  
+  newContaBancaria.save((err, result) => {
+    if(err) throw err;
+    ContaBancaria.find((e, results) => {
+      if(e) throw e;
       res.send(results);
     });
   });
@@ -44,6 +46,62 @@ router.delete('/:numeroConta', (req, res) => {
     if(err) throw err;
     res.send(`Conta ${req.params.numeroConta} removida com sucesso!`);
   });
+});
+
+router.post('/:numeroConta/deposito', (req, res) => {
+  const deposito = {
+    valor: req.body.valor,
+    contaDeOrigem: req.body.contaOrigem
+  };
+
+  ContaBancaria.findOneAndUpdate({
+    numero: deposito.contaDeOrigem 
+  }, {
+    $inc: { saldoAtual: deposito.valor }
+  }, (err, result) => {
+    if(err) throw err;
+    res.send(result);
+  });
+});
+
+router.post('/:numeroConta/saque', (req, res) => {
+  const saque = {
+    valor: req.body.valor,
+    contaDeOrigem: req.body.contaOrigem
+  };
+
+  ContaBancaria.findOneAndUpdate({
+    numero: saque.contaDeOrigem 
+  }, {
+    $inc: { saldoAtual: - saque.valor }
+  }, (err, result) => {
+    if(err) throw err;
+    res.send(result);
+  });
+});
+
+router.post('/:numeroConta/transferencia', (req, res) => {
+  const transferencia = {
+    valor: req.body.valor,
+    contaDeOrigem: req.body.contaOrigem,
+    contaDeDestino: req.body.contaDestino
+  };
+
+  ContaBancaria.findOneAndUpdate({ numero: transferencia.contaDeOrigem }, {
+    $inc: { saldoAtual: - transferencia.valor }
+  }).then(queryRes => {
+
+    ContaBancaria.findOneAndUpdate({
+      numero: transferencia.contaDeDestino
+    }, { $inc: { saldoAtual: transferencia.valor } }, 
+    (e, resultado) => {
+      if(e) throw e;
+      res.write(`A conta ${transferencia.contaDeDestino} recebeu R$ ${transferencia.valor} com sucesso!`);
+      res.write(`Saldo atual: ${resultado.saldoAtual}`);
+      res.end();
+      // res.send(`A conta ${transferencia.contaDeDestino} recebeu R$ ${transferencia.valor} com sucesso!`);
+    });
+  });  
 });
 
 module.exports = router;
