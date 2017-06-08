21ff3b61284a03c0025c15526f6bd068ede633e4
Lucas <lucas.kr1996@gmail.com>
2017-06-08 19:05:06 -0300

    Cria model para contas

---
+++ LucasGustavoKruger/oficial2/models/Conta.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+/**
+ * Nome do titular da conta
+ * CPF do titular da conta
+ * email do titular da conta
+ * Um número para esta conta que deverá ser gerado pelo servidor (Este número não é o ID)
+ * O saldo atual da conta
+ */
+var contaSchema = new Schema({
+  nomeTitular: String,
+  cpfTitular: String,
+  nrConta: String,
+  saldoAtual: Number
+});
+
+module.exports = mongoose.model('Conta', contaSchema);
