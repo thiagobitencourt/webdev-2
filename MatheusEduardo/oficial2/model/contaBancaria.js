1e0069cc96abc354def45aa6c0071ff20cbd28ee
eduardosmatheus <matheus-edu@outlook.com>
2017-06-08 19:34:41 -0300

    Implementa model de conta bancária.

---
+++ MatheusEduardo/oficial2/model/contaBancaria.js
@@ -0,0 +1,15 @@
+
+const mongoose = require('mongoose');
+const Types = mongoose.Schema.Types;
+
+const contaSchema = new mongoose.Schema({
+  _id: Types.ObjectId,
+  titular: Types.String,
+  cpf: Types.String,
+  email: Types.String,
+  numero: Types.Number,
+  saldoAtual: Types.Decimal128
+});
+
+module.exports = mongoose.model('Conta', contaSchema);
+
