a803218f6e90d3dfe989851695c31435b270b04e
MateusZabote <mateus.zabotedasilva3@gmail.com>
2017-06-08 20:24:31 -0300

    primeiro commit

---
+++ MateusZabote/Oficial2/app.js
@@ -0,0 +1,32 @@
+
+
+// var mongoose = require('mongoose');
+//     mongoose.connect('mongodb://localhost:27017/teste1');
+//    var Conta = require('./MateusZabote/Oficial2/models/conta');
+
+
+var express     = require('express');
+var app         = express();
+var bodyParser  = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8000;
+var router  = express.Router();
+
+router.use(function(req, res, next) {
+    console.log('Algo está acontecendo aqui........');
+    next();
+});
+
+router.get('/', function(req, res) {
+    res.json({ message: 'API FUNCIONANDO' });
+});
+
+router.route('/conta')
+
+app.use('/api', router);
+
+app.listen(port);
+console.log('Iniciando a aplicação na porta ' + port);
