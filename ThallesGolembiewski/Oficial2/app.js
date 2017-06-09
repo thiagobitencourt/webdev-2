9705cfe2400191572edf8a895f598100aaa2e12b
Thalles Golembiewski <gothalles@gmail.com>
2017-06-08 21:45:53 -0300

    Oficial 2

--- ThallesGolembiewski/Oficial2/app.js
+++ ThallesGolembiewski/Oficial2/app.js
@@ -8,22 +8,20 @@ const app = express();
 app.use(bodyParser.json());
 app.use('/', express.static('public'));
 
-const productApi = require('./routes/product');
+const bankAccountApi = require('./routes/BankAccount');
+const transferApi = require('./routes/Transfer);
 
 mongoose.connect('mongodb://localhost:5000/Bank');
 
 mongoose.connection.once('open', () => {
 
-  app.post('/product', productApi.criarProduct);
-  
-  app.get('/product', productApi.obterproduct);
-  
-  app.get('/product/:id', productApi.obterProduct);
-  
-  app.put('/product/:id', productApi.atualizarProduct);
-  
-  app.delete('/product/:id', productApi.removerProduct);
+  app.post('/Account', bankAccountApi.create);
+  app.get('/Account/:id', bankAccountApi.get);
+  app.put('/Account/:id', bankAccountApi.update);
+  app.delete('/Account/:id', bankAccountApi.delete);
 
+  app.post('/Transfer', transferApi.transfer);
+    
   app.listen(3000, () => console.log('Porta 3000'));
 });
 
