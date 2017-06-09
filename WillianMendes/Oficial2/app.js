5d151a0ccbe6a4a9b31b93b870b60e26265bbe76
willian mendes <willink01@hotmail.com>
2017-06-08 21:39:22 -0300

    correcao do backend

--- WillianMendes/Oficial2/app.js
+++ WillianMendes/Oficial2/app.js
@@ -21,41 +21,41 @@ app.post('/contas', function(req, res) {
   res.send(ContaRepo.obterTodasAsContas());
 });
 
-app.post('/conta/sacar/:id',function(req,res){
-	 var Conta = ContaRepo.obterConta(req.params.id);
+app.post('/conta/sacar/',function(req,res){
+	var Conta = ContaRepo.obterConta(req.body.contaOrigem);
   if(Conta) {
- 		if(ContaRepo.sacar(Conta,req.body.saque)){
-  			res.send("foi sacado: "+req.body.saque+ " ainda possui: "+Conta.saldo);
+ 		if(ContaRepo.sacar(Conta,req.body.valor)){
+  			res.send("foi sacado: "+req.body.valor+ " ainda possui: "+Conta.saldo);
   		}else{
 			res.send("valor invalido ou saldo insuficiente");
  	 }
 	} else {
-    res.status(404).send("conta nao encontrada " + req.params.id);
+    res.status(404).send("conta nao encontrada " + req.body.contaOrigem);
   }
 });
 
-app.post('/conta/depositar/:id',function(req,res){
- var Conta = ContaRepo.obterConta(req.params.id);
+app.post('/conta/depositar/',function(req,res){
+ var Conta = ContaRepo.obterConta(req.body.contaOrigem);
   if(Conta) {
-  		ContaRepo.depositar(Conta,req.body.deposito);
-  		res.send("foi adicionado: "+req.body.deposito+" a conta, total: "+conta.saldo);
+  		ContaRepo.depositar(Conta,req.body.valor);
+  		res.send("foi adicionado: "+req.body.valor+" a conta, total: "+conta.saldo);
   	} else {
-    res.status(404).send("conta nao encontrada " + req.params.id);
+    res.status(404).send("conta nao encontrada " + req.body.contaOrigem);
 	}
 });
 
 
-app.post('/conta/tranferencia/:id',function(req,res){
-	var Conta= ContaRepo.obterConta(req.params.id);
-	var dest=  ContaRepo.obterConta(req.body.destino);
+app.post('/conta/tranferencia/',function(req,res){
+	var Conta= ContaRepo.obterConta(req.body.contaOrigem);
+	var dest=  ContaRepo.obterConta(req.body.contaDestino);
 	if(Conta){
 		if(ContaRepo.tranferencia(Conta,req.body.valor,dest)){
-			res.send("tranferencia realizada com sucesso, tranferencia de "+ req.body.valor+" para conta id: "+req.body.destino);
+			res.send("tranferencia realizada com sucesso, tranferencia de "+ req.body.valor+" para conta id: "+req.body.contaDestino);
 		} else{
 			res.send("valor ou destino invalidos");
 			}
 	}else{
-		res.status(404).send("conta nao encontrada " + req.body.id);
+		res.status(404).send("conta nao encontrada " + req.body.contaOrigem);
 	}
 	
 });
