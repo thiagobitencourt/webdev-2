81918516ea199e2da3bdd3b5d08d74c251c4b862
Djiovani <djiovani.douglas@gmail.com>
2017-06-08 21:40:24 -0300

    Prova Oficial 2 - correcao models e routes - Djiovani Douglas

--- DjiovaniDouglas/Oficial2/models/BancoConta.js
+++ DjiovaniDouglas/Oficial2/models/BancoConta.js
@@ -1,49 +1,25 @@
-const conta = require('../models/BancoConta');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const ObjectId = require('mongoose').Types.ObjectId;
+const bancoContaSchema = new Schema({
+ _id: String,
+ name: {
+   type: String,
+   required: true
+ },
+ cpf: {
+   type: String,
+   required: true,
+   unique:true
 
-module.exports = {
+ },
+ email: Number,
+ account: {
+   type: Number,
+   required: true,
+   unique:true
+ },
+   balance: Number
+});
 
- create: ((req, res) => {
-   const conta = new conta({
-     _id: new ObjectId(),
-     name: req.body.name,
-     cpf: req.body.cpf,
-     email: req.body.email,
-   });
-
-   conta.save().then(conta => {
-     res.send('Conta criada!');
-   });
- }),
-
- get: ((req, res) => {
-   conta.findOne({ _id: new ObjectId(req.params.id) }, (err, conta) => {
-     if(!conta) {
-       res.status(404).send('Conta não existe');
-     } else {
-       res.send(conta);
-     }
-   });
- }),
-
- update: ((req, res) => {
-   const conta = new Conta({
-     _id: new ObjectId(req.params.id),
-     email: req.body.email
-   });
-
-   conta.findByIdAndUpdate(new ObjectId(req.params.id), conta, (err, updateconta) => {
-     if (err) throw err;
-     console.log('Conta atualizado com sucesso!');
-     res.send(updateconta);
-   });
- }),
-
- delete: ((req, res) => {
-   conta.findByIdAndRemove(new ObjectId(req.params.id), (err) => {
-     if (err) throw err;
-     res.send('Conta removida com sucesso');
-   });
- })
-};
+module.exports = mongoose.model('BancoConta', bancoContaSchema);
--- DjiovaniDouglas/Oficial2/models/Transferencia.js
+++ DjiovaniDouglas/Oficial2/models/Transferencia.js
@@ -1,39 +1,20 @@
-const Transferencia = require('../models/Transferencia');
-const conta = require('../models/BancoConta');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const ObjectId = require('mongoose').Types.ObjectId;
+const transferenciaSchema = new Schema({
+ _id: String,
+ origemConta: {
+   type: Number,
+   required: true
+ },
+ destinoConta: {
+   type: Number,
+   required: true
+ },
+ value: {
+   type: Number,
+   required: true
+}
+});
 
-module.exports = {
-
- transferencia: ((req, res) => {
-  conta.findOne({ conta: req.origemConta }, (err, origemConta) => {
-    if(!conta) {
-       res.status(404).send('Conta Origem não existe');
-       return;
-    }});
-  conta.findOne({ conta: req.destinoConta }, (err, destinoConta) => {
-    if(!conta) {
-       res.status(404).send('Conta Destino não existe');
-        return;
-   }});
-
-   const transferencia = new Transferencia({
-     _id: new ObjectId(),
-     origemConta: req.body.origemConta,
-     destinoConta: req.body.destinoConta,
-     value: req.body.value,
-   });
-
-   Transferencia.save().then(transferencia => {
-     res.send('Transferencia feita com sucesso');
-   });
-
-   origemConta.balance = origemConta.balance - transferencia.value;
-   conta.findByIdAndUpdate({ conta: req.sourceconta }, sourceconta, (err, sourcecontaUpdate) => {
-   });
-
-   destinoConta.balance = destinoConta.balance + transferencia.value;
-   conta.findByIdAndUpdate({ conta: req.sourceconta }, sourceconta, (err, sourcecontaUpdate) => {
-   });
- })
-};
+module.exports = mongoose.model('Transferencia', transferenciaSchema);
