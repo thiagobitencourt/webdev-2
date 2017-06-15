73b771aef648b218eb52cfa42ea9a08eb7648db0
Thiago R. M. Bitencourt <thiago.mbitencourt@gmail.com>
2017-06-14 21:51:36 -0300

    Adiciona ui-router

--- ThiagoBitencourt/spa/angularSPA.js
+++ ThiagoBitencourt/spa/angularSPA.js
@@ -1,5 +1,48 @@
-var app = angular.module('angularSPA', []);
+var app = angular.module('angularSPA', ['ui.router']);
+
 app.controller('mainController',
-  function($scope) {
+  function($scope, $state) {
     $scope.titulo = "Usuários";
+
+    $scope.usuarios = [
+      {
+        _id: '123',
+        username: 'fulado',
+        password: 'senha123',
+        age: 18,
+        email: 'fulano@mail.com'
+      }
+    ];
+
+    $scope.adicionarUsuario = function() {
+      console.log("vou adicionar um novo usuário");
+      $state.go('novoUsuario');
+    }
   })
+
+app.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/error');
+
+  let novoUsuarioState = {
+    name: 'novoUsuario',
+    url: '/novoUsuario',
+    templateUrl: 'novoUsuario.html'
+  }
+
+  let listaUsuariosState = {
+    name: 'listaUsuarios',
+    url: '/',
+    templateUrl: 'listaUsuarios.html',
+    controller: 'mainController'
+  }
+
+  let errorState = {
+    name: 'error',
+    url: '/error',
+    templateUrl: 'error.html'
+  }
+
+  $stateProvider.state(novoUsuarioState);
+  $stateProvider.state(listaUsuariosState);
+  $stateProvider.state(errorState);
+});
