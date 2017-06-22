c21a4edda1a974ddedef4979e642e18fb6cfd9ec
jivagocampos <jivagocampos_@hotmail.com>
2017-06-21 22:35:33 -0300

    Adicionando arquivos SPA

---
+++ JivagoCampos/spa/src/service/manterUsuarios.service.js
@@ -0,0 +1,26 @@
+angular
+.module('angularSPA')
+.service('manterUsuariosServico', function($q){
+  var listaDeUsuarios = [];
+
+  var servico = {
+    obterUsuarios: obterUsuarios,
+    salvarUsuario: salvarUsuario
+  }
+  return servico;
+
+  // É uma função assincrona, ou seja, retorna uma promessa
+  function obterUsuarios() {
+    // return $http.get('/usuarios'); // Também é assincrono, ou seja, retorna uma promessa
+    var deferred = $q.defer();
+    deferred.resolve(listaDeUsuarios);
+    return deferred.promise;
+  }
+function salvarUsuario(usuario) {
+  //return $http.post('/usuarios', usuario); // retorna
+  var deferred = $q.defer();
+  listaDeUsuarios.push(usuario);
+  deferred.resolve();
+  return deferred.promise;
+  }
+});
