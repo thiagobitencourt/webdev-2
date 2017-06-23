9fa9c40da32b82025711923b0adfc23252e36529
LeandroRogalaBritez <leandrorogala17@hotmail.com>
2017-06-23 11:47:02 -0300

    Cria component navbar

---
+++ LeandroRogala/spa/src/components/nav-bar.component.js
@@ -0,0 +1,3 @@
+angular.module('angularSPA').component('navBar',{
+  templateUrl: "/src/components/componentsView/navbar.html"
+});
--- LeandroRogala/spa/src/components/navbar.component.js
+++
@@ -1,6 +0,0 @@
-angular.module('angularSPA').component('navBar', {
-  templateUrl: 'navbar.html',
-  bindings: {
-            input: '='
-        }
-});
